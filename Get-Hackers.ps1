7e1206dd4eb96fac3dcb891d36578a5f160ec08a
Gyrferret <gyrferret117@gmail.com>
2015-04-14 11:37:37 -0700

    Created File
    Created the original file

---
+++ Get-Hackers.ps1
@@ -0,0 +1,19 @@
+﻿$event = Get-EventLog -LogName Security -InstanceId 4625 -Newest 1 
+foreach($events in $event)
+   { 
+     $regexp = '\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}'
+     ($events.message |Select-String -Pattern $regexp).Matches.Value >> C:\IPs.txt 
+     }
+
+Get-NetFirewallRule -Enabled False
+
+Set-NetFirewallSecurityFilter 
+
+New-NetFirewallRule -DisplayName RDP-TCP-Block -Action Block -Direction Inbound -Enabled True -Profile Any -Protocol TCP -RemotePort 3389
+New-NetFirewallRule -DisplayName RDP-UPD-Block -Action Block -Direction Inbound -Enabled True -Profile Any -Protocol UDP -RemotePort 3389
+Start-Sleep -Seconds 10
+$RDPTCP = Get-NetFirewallRule | where DisplayName -Like "RDP-TCP-Block"
+$RDPUDP = Get-NetFirewallRule | where DisplayName -Like "RDP-UDP-Block"
+
+$RDPTCP.DisplayName
+$RDPUDP.DisplayName
