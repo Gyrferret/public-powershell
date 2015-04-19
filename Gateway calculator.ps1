70604e7e08f77d47c0d71beff0a3669955f0ea49
Gyrferret <gyrferret117@gmail.com>
2015-04-19 14:11:45 -0700

    Gateway Fix
    Fix for when the gateway would land on the same binary IP, it would
    increment the IP erroneously.

--- Gateway calculator.ps1
+++ Gateway calculator.ps1
@@ -5,19 +5,23 @@ $remainder = $null #establishes a variable and sets its value to $null for now
 $SimOctets = [math]::DivRem($mask,8,[ref]$remainder) #Determines the amount similar octets the IP address has
 $InvSub = [math]::Pow(2,(8 - $remainder)) #determines the inverse subnet mask (Old Method)
 $Subnet = (256 - [math]::Pow(2,(8 - $remainder))) # Determines subnet mask (New Method)
-$GateIP = ([math]::floor(($IPSplit[$SimOctets] / $InvSub))) * $InvSub + 1 #determines the subnet mask (Old Method)
-#$SimOctets,$IPAddr[$SimOctets],$InvSub,$GateIP,$InvSub #Used as a checkpoint to view variable; comment out otherwise
+#$TestIP = (([math]::floor(($IPSplit[$SimOctets] / $InvSub))) * $InvSub) #determines the subnet mask (Old Method)
+If ((([math]::floor(($IPSplit[$SimOctets] / $InvSub))) * $InvSub) -eq $IPSplit[$SimOctets]) #Tests to see if the subnet starts on the current IP.
+    {$GateIP = ([math]::floor(($IPSplit[$SimOctets] / $InvSub))) * $InvSub
+    } else { 
+    $GateIP = ([math]::floor(($IPSplit[$SimOctets] / $InvSub))) * $InvSub + 1}
+#$SimOctets,$IPSplit[$SimOctets],$InvSub,$GateIP,$InvSub #Used as a checkpoint to view variable; comment out otherwise
 ######################################
 ##Logic for Determining Subnet Masks##
 ######################################
 #Clear-Variable subnet #clears the subnet variable, or else it will be inaccurate on subsequent runs of this script within the same session. Comment-out otherwise.
 if ($SimOctets -lt 3) #if condition to set the final octet as .1 if netmask less than a /24
      {
-        $Gateway = (([string]"255.") * $SimOctets)+ $subnet + (([String]".0")*(3 - $SimOctets)) #creates the gateway of any subnets less than a /24
+        $SubnetMask = (([string]"255.") * $SimOctets)+ $subnet + (([String]".0")*(3 - $SimOctets)) #creates the gateway of any subnets less than a /24
     } else { 
-        $Gateway = (([string]"255.") * $SimOctets)+ $subnet #creates the gateway of any subnets greater= than or equal to a /24
+        $Subnetmask = (([string]"255.") * $SimOctets)+ $subnet #creates the gateway of any subnets greater= than or equal to a /24
         }
-$Gateway #Used as a checkpoint to view variable; comment out otherwise
+#$SubnetMask #Used as a checkpoint to view variable; comment out otherwise
 #Clear-Variable subnet #clears the subnet variable, or else it will be inaccurate on subsequent runs of this script within the same session. Comment-out otherwise.
 ##################################
 ##Logic for Determining Gateways##
@@ -29,9 +33,14 @@ For ( $b = ($SimOctets - $SimOctets); $b -lt $SimOctets; $b++)
     }
 if ($SimOctets -lt 3)
     {
-    $SimIP + [String]$GateIP + (([String]".0")*(2 - $SimOctets)) + ([String]".1")
+    $Gateway = $SimIP + [String]$GateIP + (([String]".0")*(2 - $SimOctets)) + ([String]".1")
         } else
     {
-    $SimIP + $GateIP
+    $Gateway = $SimIP + $GateIP
     }
       Clear-Variable c #,d,IPSplit
+#Clear-Host
+
+Write-Output "IP Address: $IPAddr /$mask"
+Write-Output "Netmask: $SubnetMask"
+Write-Output "Gateway IP: $Gateway"
