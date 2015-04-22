494a9c4073422c1cb9d1036f76c3352d24bfa1f3
Gyrferret <gyrferret117@gmail.com>
2015-04-22 15:50:14 -0700

    Get-IPInfo 1.1
    Bug fixes
    Added Private IP identification
    Added Class size Identification

--- Gateway calculator.ps1
+++ Gateway calculator.ps1
@@ -9,31 +9,34 @@ param (
                ValueFromPipelineByPropertyName=$True,
                Mandatory=$True)]
             [Alias('Mask')]
-            [int[]]$SubnetMask,
-            #[String]$logfile = 'C:\Windows\Logs\Scripts\Get-IPInfoErrors.txt'#Specifies the default locaiton of the error file
+            [int[]]$SubnetMask
         )
-BEGIN{} #end of if statement
-     #end of BEGIN
+BEGIN{} #end of BEGIN
+     
 PROCESS{
-    Foreach ($IPAddress in $IPAddress) {
-        GetIPInfo -IPAddr "$IPAddress" -Mask "$SubnetMask"
-        }
+    $TestIP = "8.8.8.8"
+    If ([IPAddress]::TryParse($IPAddress,[ref]$TestIP)){
+         Foreach ($IPAddress in $IPAddress) {
+            GetIPInfo -IPAddr "$IPAddress" -Mask "$SubnetMask"
+            }
+        } else {
+            Write-Output "Please Enter a Valid IP"
+        } #End Statement
     } #End of PROCESS
-END{}
+END{} #End of End
 }
 function GetIPInfo {
 param($IPAddr, $mask)
+$Class = $null
+$remainder = $null #establishes a variable and sets its value to $null for now
+$IsPrivate = $False
 #$IPAddr #= "71.6.167.73"#Read-Host Enter a number #Prompts to enter the IP Address
 #$mask #= Read-Host Enter a netmask #Prompts to enter the subnet (e.g. /14, /22)
-#$IPTest + "8.8.8.8" //To be used later for validation
-#if ([IPAddress]::TryParse($Ipaddr,[ref]$IPtest)) //To be used later for validation
 $IPSplit = $IPAddr.Split(".")
-#$remainder = $null #establishes a variable and sets its value to $null for now
 $SimOctets = [math]::DivRem($mask,8,[ref]$remainder) #Determines the amount similar octets the IP address has
 $InvSub = [math]::Pow(2,(8 - $remainder)) #determines the inverse subnet mask (Old Method)
 $Subnet = (256 - [math]::Pow(2,(8 - $remainder))) # Determines subnet mask (New Method)
 $UsableIP = [Math]::pow(2,(((3-$simOctets)*8) + (8 - $remainder))) - 2 #Calculates the number of usable IPs in the subnet
-#$TestIP = (([math]::floor(($IPSplit[$SimOctets] / $InvSub))) * $InvSub) #determines the subnet mask (Old Method)
 If ((([math]::floor(($IPSplit[$SimOctets] / $InvSub))) * $InvSub) -eq $IPSplit[$SimOctets]) #Tests to see if the subnet starts on the current IP.
     {$GateIP = ([math]::floor(($IPSplit[$SimOctets] / $InvSub))) * $InvSub
     } else { 
@@ -72,20 +75,31 @@ if ($SimOctets -lt 3)
     }
       #Clear-Variable c #,d,IPSplit
 
+If ($IPSplit[0] -eq 10) #Determines Class and if IP is Private
+    { $IsPrivate = $True
+      $Class = "A"
+    } elseif ($IPSplit[0] -eq 172 -and ( 16 -ge $IPSplit[1] -le 31))
+        { $IsPrivate = $True
+          $Class = "B"
+            } elseif ($IPSplit[0] -eq 192 -and $IPSplit[1] -eq 168)
+            { $IsPrivate = $True
+              $Class = "C"}
 
 $PC = New-Object -TypeName PSObject
-$PC | Add-Member -MemberType NoteProperty -name "IP Address" -value $IPAddr 
-$PC | Add-Member -MemberType NoteProperty -name "Subnet Mask" -value $mask
+$PC | Add-Member -MemberType NoteProperty -name "IPAddress" -value $IPAddr 
+$PC | Add-Member -MemberType NoteProperty -name "SubnetMask" -value $mask
 $PC | Add-Member -MemberType NoteProperty -name "Netmask" -value $SubnetMask
-$PC | Add-Member -MemberType NoteProperty -name  "First Usable IP" -value $Gateway
-$PC | Add-Member -MemberType NoteProperty -name "Last Usable IP" $FinalIP
-$PC | Add-Member -MemberType NoteProperty -name "IP Range" -value $FinalIP
-$PC | Add-Member -MemberType NoteProperty -name "Broadcast IP"  -value $BroadIP
-$PC | Add-Member -MemberType NoteProperty -name "Usable IPs" -value $UsableIP
+$PC | Add-Member -MemberType NoteProperty -name  "FirstUsableIP" -value $Gateway
+$PC | Add-Member -MemberType NoteProperty -name "LastUsableIP" $FinalIP
+$PC | Add-Member -MemberType NoteProperty -name "BroadcastIP"  -value $BroadIP
+$PC | Add-Member -MemberType NoteProperty -name "IPRange" -value ("$Gateway - $FinalIP")
+$PC | Add-Member -MemberType NoteProperty -name "UsableIPs" -value $UsableIP
+$PC | Add-Member -MemberType NoteProperty -name "IsPrivate" -value $IsPrivate
+$PC | Add-Member -MemberType NoteProperty -name "Class" -value $Class
 
-$PC
+$PC 
 } #End of Function
-Get-IPInfo -IPAddress 71.6.167.73 -SubnetMask 25
+Get-IPInfo -IPAddress 192.168.2.5 -SubnetMask 24 
 
 #New-Alias GIP Get-IPInfo
 
