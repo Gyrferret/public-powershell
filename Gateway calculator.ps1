5e3a7d6e5a872e749e9d7094216360474f2a58a8
Gyrferret <gyrferret117@gmail.com>
2015-04-22 10:38:35 -0700

    Get-IPInfo 1.0
    Initial Release!

--- Gateway calculator.ps1
+++ Gateway calculator.ps1
@@ -10,38 +10,25 @@ param (
                Mandatory=$True)]
             [Alias('Mask')]
             [int[]]$SubnetMask,
-            [String]$logfile = 'C:\Windows\Logs\Scripts\Get-IPInfoErrors.txt'#Specifies the default locaiton of the error file
+            #[String]$logfile = 'C:\Windows\Logs\Scripts\Get-IPInfoErrors.txt'#Specifies the default locaiton of the error file
         )
-BEGIN{
-    if (!(test-path $logfile)) #tests path of log file
-       { 
-        try { 
-            New-Item $logfile -ea SilentlyContinue #attempts to make one if one isn't present
-            } catch {
-            Write-Output "Unable to create Log File" #message if unable to make one
-            }
-        } else {
-        try {
-            Remove-Item $logfile -ea SilentlyContinue #attempts to remove one if present
-            New-Item $logfile -ea SilentlyContinue #attempts to make one after removal
-            } catch {
-            Write-Output "Unable to remove old log file, or create new one" #message if unable to do either
-            }
-        } #end of if statement
-    } #end of BEGIN
+BEGIN{} #end of if statement
+     #end of BEGIN
 PROCESS{
-    Foreach ($IPAddress in $IPAddress)
-        GetIPInfo -IPAddr $IPAddress -Mask $NetMask
+    Foreach ($IPAddress in $IPAddress) {
+        GetIPInfo -IPAddr "$IPAddress" -Mask "$SubnetMask"
+        }
     } #End of PROCESS
+END{}
 }
 function GetIPInfo {
-param($IPAddr = 127.0.0.1, $mask = 255.255.255.0 )
-$IPAddr = "71.6.167.73"#Read-Host Enter a number #Prompts to enter the IP Address
-[int]$mask = Read-Host Enter a netmask #Prompts to enter the subnet (e.g. /14, /22)
+param($IPAddr, $mask)
+#$IPAddr #= "71.6.167.73"#Read-Host Enter a number #Prompts to enter the IP Address
+#$mask #= Read-Host Enter a netmask #Prompts to enter the subnet (e.g. /14, /22)
 #$IPTest + "8.8.8.8" //To be used later for validation
 #if ([IPAddress]::TryParse($Ipaddr,[ref]$IPtest)) //To be used later for validation
 $IPSplit = $IPAddr.Split(".")
-$remainder = $null #establishes a variable and sets its value to $null for now
+#$remainder = $null #establishes a variable and sets its value to $null for now
 $SimOctets = [math]::DivRem($mask,8,[ref]$remainder) #Determines the amount similar octets the IP address has
 $InvSub = [math]::Pow(2,(8 - $remainder)) #determines the inverse subnet mask (Old Method)
 $Subnet = (256 - [math]::Pow(2,(8 - $remainder))) # Determines subnet mask (New Method)
@@ -96,10 +83,11 @@ $PC | Add-Member -MemberType NoteProperty -name "IP Range" -value $FinalIP
 $PC | Add-Member -MemberType NoteProperty -name "Broadcast IP"  -value $BroadIP
 $PC | Add-Member -MemberType NoteProperty -name "Usable IPs" -value $UsableIP
 
+$PC
 } #End of Function
+Get-IPInfo -IPAddress 71.6.167.73 -SubnetMask 25
 
+#New-Alias GIP Get-IPInfo
 
-New-Alias GIP Get-IPInfo
-
-Export-ModuleMember -Function Get-IPInfo
-Export-ModuleMember -alias GIP
+#Export-ModuleMember -Function Get-IPInfo
+#Export-ModuleMember -alias GIP
