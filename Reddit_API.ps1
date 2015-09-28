c6874e23ef1c8ac9b43e999db13e7fce37cf8fdc
Gyrferret <dv@noloc.us>
2015-09-27 21:35:26 -0500

    Reddit API via Powershell

---
+++ Reddit_API.ps1
@@ -0,0 +1,18 @@
+﻿$user = "Gyrferret"
+$password = "password" 
+$clientID = "clientID"
+$clientSecret = "clientsecret"
+
+$jsonc = $credentials 
+$jsonh = $header | ConvertTo-Json
+
+($clientID + ":" + $clientSecret)
+
+$token = Invoke-RestMethod -Method Post -Uri "https://www.reddit.com/api/v1/access_token" -Body $jsonc -ContentType 'application/x-www-form-urlencoded' -Credential $clientID
+
+$header = @{ 
+    authorization = $token.token_type + " " + $token.access_token
+    }
+
+$header
+Invoke-RestMethod -uri "https://oauth.reddit.com/api/v1/me" -Headers $header -UserAgent "Powershell Test"
