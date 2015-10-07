85253fff0584b8cfc567706aebd2d4422000cbc3
Gyrferret <dv@noloc.us>
2015-10-06 21:58:42 -0500

    Reddit_API 1.1
    Stuff

--- Reddit_API.ps1
+++ Reddit_API.ps1
@@ -1,18 +1,23 @@
-﻿$user = "Gyrferret"
-$password = "password" 
-$clientID = "clientID"
-$clientSecret = "clientsecret"
+﻿$user = "user" #User's Username
+$password = "password" #User's Password
+$clientID = "clientid" #User's Application Client ID
+$clientSecret = "clientsecret" #User's Application Secret
 
-$jsonc = $credentials 
-$jsonh = $header | ConvertTo-Json
 
-($clientID + ":" + $clientSecret)
-
-$token = Invoke-RestMethod -Method Post -Uri "https://www.reddit.com/api/v1/access_token" -Body $jsonc -ContentType 'application/x-www-form-urlencoded' -Credential $clientID
-
-$header = @{ 
-    authorization = $token.token_type + " " + $token.access_token
+$credentials = @{ #builds body for submission
+    username = $user #
+    grant_type = "password" #Use "password" since this is a personal script
+    password = $password 
     }
 
-$header
-Invoke-RestMethod -uri "https://oauth.reddit.com/api/v1/me" -Headers $header -UserAgent "Powershell Test"
+
+$token = Invoke-RestMethod -Method Post -Uri "https://www.reddit.com/api/v1/access_token" -Body $credentials -ContentType 'application/x-www-form-urlencoded' -Credential $clientID #Retrieves token
+
+$header = @{
+    authorization = $token.token_type + " " + $token.access_token #authorization header to be used for all API calls
+    }
+$Agent = "Windows Powershell 5.0" #Agent String
+
+$Account = Invoke-RestMethod -Uri https://oauth.reddit.com/api/v1/me  -Headers $header -UserAgent $Agent #retrieves account information
+
+
