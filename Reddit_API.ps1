0143e8931d07f337e887c430d170d25eaa1e5425
Gyrferret <dv@noloc.us>
2015-10-10 11:19:30 -0500

    Reddit API 0.1
    Ability to look up user info.

--- Reddit_API.ps1
+++ Reddit_API.ps1
@@ -1,23 +1,45 @@
-﻿$user = "user" #User's Username
-$password = "password" #User's Password
-$clientID = "clientid" #User's Application Client ID
-$clientSecret = "clientsecret" #User's Application Secret
+﻿function Get-RedditToken {
+$user = "user"
+$password = "password" 
+$clientID = "clientID"
+$clientSecret = ConvertTo-SecureString "secret" -AsPlainText -Force
+$creds = New-Object -TypeName System.management.Automation.PSCredential -ArgumentList $clientID, $clientSecret 
 
-
-$credentials = @{ #builds body for submission
-    username = $user #
-    grant_type = "password" #Use "password" since this is a personal script
-    password = $password 
+$credentials = @{
+    grant_type = "password"
+    username = $user
+    password = $password
     }
+$env:token = Invoke-RestMethod -Method Post -Uri "https://www.reddit.com/api/v1/access_token" -Body $credentials -ContentType 'application/x-www-form-urlencoded' -Credential $creds
+}
+Get-RedditToken
 
 
-$token = Invoke-RestMethod -Method Post -Uri "https://www.reddit.com/api/v1/access_token" -Body $credentials -ContentType 'application/x-www-form-urlencoded' -Credential $clientID #Retrieves token
-
-$header = @{
-    authorization = $token.token_type + " " + $token.access_token #authorization header to be used for all API calls
-    }
-$Agent = "Windows Powershell 5.0" #Agent String
-
-$Account = Invoke-RestMethod -Uri https://oauth.reddit.com/api/v1/me  -Headers $header -UserAgent $Agent #retrieves account information
-
-
+function Get-RedditUser {
+    param(
+    [string]$username,
+    [switch]$info
+    )
+    if (!($token))
+        {Get-Token}
+        $header = @{ 
+            authorization = $token.token_type + " " + $token.access_token
+         }
+    if ($info)
+        {$accinfo = (Invoke-RestMethod -uri "https://oauth.reddit.com/user/$username/about" -Headers $header -UserAgent "Powershell Test").data
+        $acccreate = (Get-Date -date 1.1.1970).AddSeconds(($accinfo.created))
+        $pc = New-Object -TypeName PSObject
+        $pc | Add-Member -MemberType NoteProperty -Name "Account Name" -Value ($accinfo.name)
+        $pc | Add-Member -MemberType NoteProperty -Name "Link Karma" -Value ($accinfo.link_karma)
+        $pc | Add-Member -MemberType NoteProperty -Name "Comment karma" -Value ($accinfo.comment_karma)
+        $pc | Add-Member -MemberType NoteProperty -Name "Combined karma" -Value (($accinfo.comment_karma) + ($accinfo.link_karma))
+        $pc | Add-Member -MemberType NoteProperty -Name "Account Created" -Value ($acccreate)
+        $pc | Add-Member -MemberType NoteProperty -Name "Gilded" -Value ($accinfo.is_gold)
+        $pc | Add-Member -MemberType NoteProperty -Name "Mod" -Value ($accinfo.is_mod)
+        $pc
+        }
+        else {
+            (Invoke-RestMethod -uri "https://oauth.reddit.com/user/$username" -Headers $header -UserAgent "Powershell Test").data.children.data
+            }
+}
+Get-RedditUser -username Reddit_User -info
