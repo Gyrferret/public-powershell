1489fc84e01632a8e550ee99fac418d7be138e03
Gyrferret <dv@noloc.us>
2015-11-15 19:19:10 -0600

    Get-Password
    Initial Creation. All credit due to
    http://www.peterprovost.org/blog/2007/06/22/Quick-n-Dirty-PowerShell-Password-Generator/

---
+++ Get-Password.ps1
@@ -0,0 +1,23 @@
+﻿function Get-Password {
+param( 
+[int] $length = 12,
+[switch] $complex,
+[switch] $nonalphanumeric
+)
+If ($nonalphanumeric) 
+    {[string]$cBase = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*_-+=`|\)(}{][:;><,.?`'`"" }
+     elseif ($complex) {
+     [string]$cBase = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*"
+     } else {
+     [string]$cBase = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    }
+$bytes = new-object "System.Byte[]" $length
+$crypto = new-object System.Security.Cryptography.RNGCryptoServiceProvider
+$crypto.GetNonZeroBytes($bytes)
+$password = ""
+for( $i=0; $i -lt $length; $i++ )
+{
+$password += $cBase[$bytes[$i] % $cBase.Length]
+}
+$password
+}
